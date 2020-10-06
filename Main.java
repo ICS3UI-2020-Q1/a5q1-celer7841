b0f7a242bacd2433af71a888b2f787eb5bca97d2
celer7841 <celer7841@wrdsb.ca>
2020-10-06 13:58:16 +0000

    finished

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author celer7841
  */
 public class Main {
 
@@ -9,7 +10,16 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create the counting variable 
+   System.out.println("Please enter an integer to create a multiplication table for");
+   Scanner input = new Scanner(System.in);
+    int num = input.nextInt();
+    
+      // print the number
+    for(int count = 1; count <= 12; count = count + 1){
+      int multiplication = count * num;
+      System.out.println(count + " x " + num +  " = " + multiplication);
+    }
     
   }
 }
